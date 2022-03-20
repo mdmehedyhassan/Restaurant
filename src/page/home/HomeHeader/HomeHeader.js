7f11e77c1038ed46fc2de9dcd6b475ecd0bbfe59
unknown <mdmehedyh55@gamil.com>
2022-03-20 23:32:52 +0600

    add aos

--- src/page/home/HomeHeader/HomeHeader.js
+++ src/page/home/HomeHeader/HomeHeader.js
@@ -8,9 +8,9 @@ const HomeHeader = () => {
         <div className="container">
             <div className="row p-3 align-items-center">
                 <div className="col-md-6 mt-2">
-                    <h1 style={{ fontSize: 60, textShadow: '1px 1px 15px #eb6434, -1px -1px 15px #eb6434', color: 'black', }} className="fw-bolder">Chinese Restaurant</h1>
-                    <p style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quod adipisci nisi, cumque, earum vitae, repellat maiores tempore nemo voluptatem debitis assumenda? Itaque numquam aut expedita assumenda, hic vel aliquid.</p>
-                    <Link to="/menu" className="link-btn-style">
+                    <h1 data-aos="fade-up" data-aos-duration="1000" style={{ fontSize: 60, textShadow: '1px 1px 15px #eb6434, -1px -1px 15px #eb6434', color: 'black', }} className="fw-bolder">Chinese Restaurant</h1>
+                    <p data-aos="fade-up" data-aos-duration="2000" style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quod adipisci nisi, cumque, earum vitae, repellat maiores tempore nemo voluptatem debitis assumenda? Itaque numquam aut expedita assumenda, hic vel aliquid.</p>
+                    <Link data-aos="fade-up" data-aos-duration="3000" to="/menu" className="link-btn-style">
                         SEE THE MENU
                     </Link>
 
